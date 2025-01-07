b0b1a2672626bfd5a22cafc5e770060e97411f07
Sanju S Nath <sanjusnath9645@gmail.com>
2025-01-07 17:33:21 +0530

    Font awsome added

--- package/src/js/Plotter.js
+++ package/src/js/Plotter.js
@@ -14,6 +14,14 @@ class Plotter {
     this.charts = {};
     this.enhancedChartType = new Typify(this);
     Styleblast(); 
+    this.#includeFontAwesome();
+  }
+
+  #includeFontAwesome() {
+    const link = document.createElement('link');
+    link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css';
+    link.rel = 'stylesheet';
+    document.head.appendChild(link);
   }
 
   #customBackgroundPlugin(backgroundColor) {
